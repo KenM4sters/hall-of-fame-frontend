19cdc99760f3dd73c3a379702c2c7f2149beb666
KenM4sters <EHCKenway@outlook.com>
2024-02-12 10:00:31 +0000

    accessing each character onClick()

--- src/r3f.jsx
+++ src/r3f.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls, Sparkles, Stats } from "@react-three/drei";
@@ -7,10 +7,9 @@ import * as THREE from 'three'
 const R3f = ({ instanceCount, data }) => {
 
   const characterRef = useRef();
+  const [characterSelected, setCharacterSelected] = useState(undefined);
+  console.log(characterSelected);
 
-  const handleCharacterClick = () => {
-    console.log(characterRef.current.position);
-  }
 
   // const meshPositions = Array.from({ length: instanceCount }, (_, index) => Array.from({length: 3}, (_, i) => Math.random() * 10));
   // console.log(data.content != undefined ? data.content[0].imageUrl : "not defined");
@@ -20,11 +19,12 @@ const R3f = ({ instanceCount, data }) => {
     meshPositions.push([(i*3) - 4.5, 0.5, 0])
   }
 
+  for(let i = 0; i < data?.content?.length; i++) {
+    data.content[i]["texture"] = new useLoader(THREE.TextureLoader, data.content != undefined ? data.content[i].imageUrl : "src/assets/unnamed.jpg");
+  }
 
+  // const textureArray = Array.from({ length: data.numberOfElements}, (_, index) => new useLoader(THREE.TextureLoader, data.content != undefined ? data.content[index].imageUrl : "src/assets/unnamed.jpg"))
 
-
-  const textureArray = Array.from({ length: data.numberOfElements}, (_, index) => new useLoader(THREE.TextureLoader, data.content != undefined ? data.content[index].imageUrl : "src/assets/unnamed.jpg"))
-  console.log(textureArray);
 
 
   
@@ -40,16 +40,16 @@ const R3f = ({ instanceCount, data }) => {
         }}
       >
         <OrbitControls />
-        <Sparkles size={10} color={'orange'} scale={[10]} speed={[0.2]} />
+        {/* <Sparkles size={10} color={'orange'} scale={[10]} speed={[0.2]} /> */}
         {meshPositions.map((position, index) => (
-          <mesh key={index} position={position} ref={characterRef} onClick={() => {handleCharacterClick()} } >
+          <mesh key={index} position={position} onClick={() => {setCharacterSelected(data?.content[index])} } >
             <boxGeometry attach="geometry" args={[1.8, 1.8, 0.2]} />
-            <meshBasicMaterial attach="material-0" color="#ffffff"/>
-            <meshBasicMaterial attach="material-1" color="#ffffff"/>
-            <meshBasicMaterial attach="material-2" color="#ffffff"/>
-            <meshBasicMaterial attach="material-3" color="#ffffff"/>
-            <meshBasicMaterial attach="material-4" map={textureArray[index]}  />
-            <meshBasicMaterial attach="material-5" color="#ffffff"/>
+            <meshBasicMaterial attach="material-0" color="#101010"/>
+            <meshBasicMaterial attach="material-1" color="#101010"/>
+            <meshBasicMaterial attach="material-2" color="#101010"/>
+            <meshBasicMaterial attach="material-3" color="#101010"/>
+            <meshBasicMaterial attach="material-4" map={data?.content[index].texture}  />
+            <meshBasicMaterial attach="material-5" color="#101010"/>
           </mesh>
         ))} 
       </Canvas>
